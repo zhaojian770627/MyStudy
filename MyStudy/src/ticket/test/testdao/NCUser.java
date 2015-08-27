f650d07f815affbd96c03363e18e5a838f456e82
zhaojianc <zhaojianc@yonyou.com>
2015-08-27 13:31:52 +0800

    object test
    Change-Id: I696a33204e38d1584769fa48004024a3d167c5b9
    Signed-off-by: zhaojianc <zhaojianc@yonyou.com>

---
+++ MyStudy/src/ticket/test/testdao/NCUser.java
@@ -0,0 +1,20 @@
+package ticket.test.testdao;
+
+public class NCUser {
+	String id;
+	String name;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	
+}
