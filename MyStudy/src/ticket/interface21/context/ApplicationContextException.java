783a885e8524a3d7b977a199277a82b99723257e
zhaojian <zhaojian770627@163.com>
2015-09-08 21:59:44 +0800

    Signed-off-by: zhaojian <zhaojian770627@163.com>

---
+++ MyStudy/src/ticket/interface21/context/ApplicationContextException.java
@@ -0,0 +1,27 @@
+
+
+package ticket.interface21.context;
+
+import ticket.interface21.core.NestedRuntimeException;
+
+/**
+ *
+ * @author  rod
+ * @version 
+ */
+public class ApplicationContextException extends NestedRuntimeException {
+
+    /**
+	 * Constructs an <code>ApplicationContextException</code> with the specified detail message.
+     * @param msg the detail message.
+     */
+    public ApplicationContextException(String msg) {
+        super(msg);
+    }
+	
+	public ApplicationContextException(String msg, Throwable t) {
+        super(msg, t);
+    }
+}
+
+
