fa228636d286cf479596c4d60c5804c6ef110a12
zhaojian770627 <zhaojian770627@163.com>
2015-10-15 22:37:11 +0800

    add file
    Signed-off-by: zhaojian770627 <zhaojian770627@163.com>

--- JBossWeb/src/ejb/HelloBean.java
+++ JBossWeb/src/ejb/HelloBean.java
@@ -1,50 +1,74 @@
 package ejb;
 
-import java.rmi.RemoteException;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
-import javax.ejb.EJBException;
 import javax.ejb.SessionBean;
 import javax.ejb.SessionContext;
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
 
 /**
  * 演示无状态会话Bean
+ * 
  * @author zhaojian
  *
  */
 public class HelloBean implements SessionBean {
 	private SessionContext ctx;
-	
-	public void ejbCreate()  {
+
+	public void ejbCreate() {
 		System.out.println("ejbCreate");
 	}
+
 	@Override
-	public void ejbActivate()  {
+	public void ejbActivate() {
 		System.out.println("ejbActivate");
 	}
 
 	@Override
-	public void ejbPassivate()  {
+	public void ejbPassivate() {
 		System.out.println("ejbPassivate");
 
 	}
 
 	@Override
-	public void ejbRemove()  {
+	public void ejbRemove() {
 		System.out.println("ejbRemove");
 
 	}
 
 	@Override
-	public void setSessionContext(SessionContext ctx)  {
-		this.ctx=ctx;
+	public void setSessionContext(SessionContext ctx) {
+		this.ctx = ctx;
 	}
 
 	/**
 	 * 业务方法
+	 * 
 	 * @return
 	 */
-	public String hello(){
+	public String hello() {
 		System.out.println("hello()");
+		try {
+			Context ctx = new InitialContext();
+			DataSource datasource = (DataSource) ctx.lookup("java:comp/env/jdbc/OracleDS");
+			Connection con = datasource.getConnection();
+			Statement stat = con.createStatement();
+			ResultSet rs = stat.executeQuery("select count(1) from sm_user");
+			if (rs.next())
+				return rs.getString(1);
+		} catch (NamingException e) {
+			return e.getMessage();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
 		return "Hello,World";
 	}
 }
