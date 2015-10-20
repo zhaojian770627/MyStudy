6c978ebe9264cb6edaad9439bd196b936ec3721f
zhaojianc <zhaojianc@yonyou.com>
2015-10-20 16:38:12 +0800

    modify
    Change-Id: I8fb48599b989618c0ba404acaf8b1202ff390ec0
    Signed-off-by: zhaojianc <zhaojianc@yonyou.com>

--- MyStudy/src/classloader/ClassLoaderTree.java
+++ MyStudy/src/classloader/ClassLoaderTree.java
@@ -1,12 +1,15 @@
 package classloader;
 
+import java.io.IOException;
+
 public class ClassLoaderTree {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		ClassLoader loader = ClassLoaderTree.class.getClassLoader();
 		while (loader != null) {
 			System.out.println(loader.toString());
 			loader = loader.getParent();
 		}
+		System.in.read();
 	}
 }
