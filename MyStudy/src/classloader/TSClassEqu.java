09fd62c67d52741bb694b97796b3e112b132060a
zhaojianc <zhaojianc@yonyou.com>
2015-10-21 16:55:23 +0800

    add file
    Change-Id: I56159fb4c0c6b889e9a072074c6de8493725c247
    Signed-off-by: zhaojianc <zhaojianc@yonyou.com>

--- MyStudy/src/classloader/TSClassEqu.java
+++ MyStudy/src/classloader/TSClassEqu.java
@@ -1,5 +1,6 @@
 package classloader;
 
+import java.io.IOException;
 import java.lang.reflect.Method;
 
 /**
@@ -9,20 +10,30 @@ import java.lang.reflect.Method;
  */
 public class TSClassEqu {
 	public void testClassIdentity() { 
-	    String classDataRootPath = "C:\\workspace\\Classloader\\classData"; 
+	    String classDataRootPath = "E:\\GitWorkspace\\MyStudy\\MyStudy\\bin\\classloader"; 
 	    FileSystemClassLoader fscl1 = new FileSystemClassLoader(classDataRootPath); 
 	    FileSystemClassLoader fscl2 = new FileSystemClassLoader(classDataRootPath); 
-	    String className = "com.example.Sample"; 	
+	    String className = "classloader.Sample"; 	
 	    try { 
 	        Class<?> class1 = fscl1.loadClass(className); 
 	        Object obj1 = class1.newInstance(); 
+	        System.out.println(ClassLoaderAnalyzer.showClassLoaderHierarchy(obj1, "zj", "\n", "-"));
 	        Class<?> class2 = fscl2.loadClass(className); 
-	        Object obj2 = class2.newInstance(); 
+	        Object obj2 = class2.newInstance();
+	        obj1=obj2;
+	        System.out.println(ClassLoaderAnalyzer.showClassLoaderHierarchy(obj2, "zj", "\n", "-"));
 	        Method setSampleMethod = class1.getMethod("setSample", java.lang.Object.class); 
 	        setSampleMethod.invoke(obj1, obj2); 
+	        int i=0;
+	        i++;
 	    } catch (Exception e) { 
 	        e.printStackTrace(); 
 	    } 
 	 }
+	
+	public static void main(String[] args) throws IOException {
+		TSClassEqu e=new TSClassEqu();
+		e.testClassIdentity();
+	}
 }
 
