47bbc242401f39aa218afb250fc993105b52ac83
zhaojianc <zhaojianc@yonyou.com>
2015-10-28 16:41:01 +0800

    测试调试
    Change-Id: I97b4501127c2095a564206638d440f66a29f6de1
    Signed-off-by: zhaojianc <zhaojianc@yonyou.com>

---
+++ MyStudy/src/jvm/MethodTraceTest.java
@@ -0,0 +1,27 @@
+package jvm;
+
+/**
+ * 此类的目的是测试JVM调试
+ * 用c文件夹下文件建立dll文件
+ * 用java执行代码如下，见c下面的图
+ * 
+ * 
+ * @author zhaojianc
+ *
+ */
+public class MethodTraceTest{
+
+	public static void main(String[] args){
+		MethodTraceTest test = new MethodTraceTest();
+		test.first();
+		test.second();
+	}
+	
+	public void first(){
+		System.out.println("=> Call first()");
+	}
+	
+	public void second(){
+		System.out.println("=> Call second()");
+	}
+}
