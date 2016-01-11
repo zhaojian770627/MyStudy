808d205958e83e4b87e983d4975681ef33b083d2
zhaojianc <zhaojianc@yonyou.com>
2016-01-11 13:08:38 +0800

    add file
    Change-Id: Ib6836f4b085e5f0d0a093b2fdcf9d5de3ea56c8b

---
+++ MyStudy/src/networksocket/five/TCPProtocol.java
@@ -0,0 +1,12 @@
+package networksocket.five;
+
+import java.io.IOException;
+import java.nio.channels.SelectionKey;
+
+public interface TCPProtocol {
+	void handleAccept(SelectionKey key) throws IOException;
+
+	void handleRead(SelectionKey key) throws IOException;
+
+	void handleWrite(SelectionKey key) throws IOException;
+}
