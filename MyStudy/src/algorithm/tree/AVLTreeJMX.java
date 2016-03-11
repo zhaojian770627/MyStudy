a031641a70a134f90dd9cdc14c95671176782c15
zhaojianc <zhaojianc@yonyou.com>
2016-03-11 11:12:04 +0800

    add
    Change-Id: I2742b98a5699adedde29469841690b1bed145087
    Signed-off-by: zhaojianc <zhaojianc@yonyou.com>

--- MyStudy/src/algorithm/tree/AVLTreeJMX.java
+++ MyStudy/src/algorithm/tree/AVLTreeJMX.java
@@ -22,8 +22,6 @@ import javax.swing.tree.DefaultTreeModel;
 import javax.swing.tree.TreeNode;
 import javax.swing.tree.TreePath;
 
-import com.sun.jdmk.comm.HtmlAdaptorServer;
-
 public class AVLTreeJMX implements AVLTreeJMXMBean {
 	class AVLNode {
 		int value;
