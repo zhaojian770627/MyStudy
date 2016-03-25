72760417c3739ae16c32d366441ff7f8d1df22bb
zhaojianc <zhaojianc@yonyou.com>
2016-03-25 14:29:42 +0800

    add js
    Change-Id: I280c91943feb0d2d523d482c07b088f92d799ca2
    Signed-off-by: zhaojianc <zhaojianc@yonyou.com>

---
+++ StudyWeb/WebContent/frameworkjs/lib/data.js
@@ -0,0 +1,31 @@
+define(function() {
+	// Do setup work here
+
+	// var viewModel = [ {
+	// mealName : 'Standard',
+	// description : 'Dry crusts of bread',
+	// extraCost : 0
+	// }, {
+	// mealName : 'Premium',
+	// description : 'Fresh bread with cheese',
+	// extraCost : 9.95
+	// }, {
+	// mealName : 'Deluxe',
+	// description : 'Caviar and vintage Dr Pepper',
+	// extraCost : 18.50
+	// } ];
+
+	return [ {
+		mealName : 'Standard',
+		description : 'Dry crusts of bread',
+		extraCost : 0
+	}, {
+		mealName : 'Premium',
+		description : 'Fresh bread with cheese',
+		extraCost : 9.95
+	}, {
+		mealName : 'Deluxe',
+		description : 'Caviar and vintage Dr Pepper',
+		extraCost : 18.50
+	} ];
+});
