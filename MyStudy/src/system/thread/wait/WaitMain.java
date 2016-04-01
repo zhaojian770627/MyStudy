5ff46eda01ccfc64c002ed224b486d92171ff547
zhaojian770627 <zhaojian770627@163.com>
2016-04-01 20:23:24 +0800

    add
    Signed-off-by: zhaojian770627 <zhaojian770627@163.com>

---
+++ MyStudy/src/system/thread/wait/WaitMain.java
@@ -0,0 +1,43 @@
+package system.thread.wait;
+
+public class WaitMain {
+    public static Object object = new Object();
+    public static void main(String[] args) {
+        Thread1 thread1 = new Thread1();
+        Thread2 thread2 = new Thread2();
+         
+        thread1.start();
+         
+        try {
+            Thread.sleep(200);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+         
+        thread2.start();
+    }
+     
+    static class Thread1 extends Thread{
+        @Override
+        public void run() {
+            synchronized (object) {
+                try {
+                    object.wait();
+                } catch (InterruptedException e) {
+                }
+                System.out.println("线程"+Thread.currentThread().getName()+"获取到了锁");
+            }
+        }
+    }
+     
+    static class Thread2 extends Thread{
+        @Override
+        public void run() {
+            synchronized (object) {
+                object.notify();
+                System.out.println("线程"+Thread.currentThread().getName()+"调用了object.notify()");
+            }
+            System.out.println("线程"+Thread.currentThread().getName()+"释放了锁");
+        }
+    }
+}
