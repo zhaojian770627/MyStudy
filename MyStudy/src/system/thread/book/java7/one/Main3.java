db552ee2dd20fa85e1deb14c7157b9c4f46409b2
zhaojian770627 <zhaojian770627@163.com>
2016-06-09 18:43:14 +0800

    add
    Signed-off-by: zhaojian770627 <zhaojian770627@163.com>

---
+++ MyStudy/src/system/thread/book/java7/one/Main3.java
@@ -0,0 +1,33 @@
+package system.thread.book.java7.one;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ *  Main class of the sample. Launch the PrimeGenerator, waits 
+ *  five seconds and interrupts the Thread
+ */
+public class Main3 {
+
+	/**
+	 * Main method of the sample. Launch the PrimeGenerator, waits
+	 * five seconds and interrupts the Thread
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+		// Launch the prime numbers generator
+		Thread task=new PrimeGenerator();
+		task.start();
+		
+		// Wait 5 seconds
+		try {
+			TimeUnit.SECONDS.sleep(5);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		
+		// Interrupt the prime number generator
+		task.interrupt();
+	}
+
+}
