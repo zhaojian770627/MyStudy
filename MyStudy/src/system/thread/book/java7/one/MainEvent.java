ecdbec4ee65781703cca3e3b27d1cf3849afd60b
zhaojian770627 <zhaojian770627@163.com>
2016-06-10 20:01:48 +0800

    add
    Signed-off-by: zhaojian770627 <zhaojian770627@163.com>

---
+++ MyStudy/src/system/thread/book/java7/one/MainEvent.java
@@ -0,0 +1,34 @@
+package system.thread.book.java7.one;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+/**
+ * Main class of the example. Creates three WriterTaks and a CleanerTask 
+ *
+ */
+public class MainEvent {
+
+	/**
+	 * Main method of the example. Creates three WriterTasks and a CleanerTask
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		// Creates the Event data structure
+		Deque<Event> deque=new ArrayDeque<Event>();
+		
+		// Creates the three WriterTask and starts them
+		WriterTask writer=new WriterTask(deque);
+		for (int i=0; i<3; i++){
+			Thread thread=new Thread(writer);
+			thread.start();
+		}
+		
+		// Creates a cleaner task and starts them
+		CleanerTask cleaner=new CleanerTask(deque);
+		cleaner.start();
+
+	}
+
+}
