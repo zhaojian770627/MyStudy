59e1906d4bff9309c926d6e652ac0fa68ec7b03e
zhaojianc <zhaojianc@yonyou.com>
2016-06-13 23:03:27 +0800

    add
    Signed-off-by: zhaojianc <zhaojianc@yonyou.com>

---
+++ MyStudy/src/system/thread/book/java7/one/Task.java
@@ -0,0 +1,19 @@
+package system.thread.book.java7.one;
+
+/**
+ * Runnable class than throws and Exception
+ *
+ */
+public class Task implements Runnable {
+
+
+	/**
+	 * Main method of the class
+	 */
+	@Override
+	public void run() {
+		// The next instruction always throws and exception
+		int numero=Integer.parseInt("TTT");
+	}
+
+}
