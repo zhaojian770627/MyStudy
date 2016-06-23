81a303541bdfae06f4c2134fa2b3d7a5548b5839
zhaojianc <zhaojianc@yonyou.com>
2016-06-23 20:37:55 +0800

    add
    Signed-off-by: zhaojianc <zhaojianc@yonyou.com>

---
+++ MyStudy/src/system/thread/book/java7/two/s4/PrintQueue.java
@@ -0,0 +1,34 @@
+package system.thread.book.java7.two.s4;
+
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * This class simulates a print queue
+ *
+ */
+public class PrintQueue {
+
+	/**
+	 * Lock to control the access to the queue.
+	 */
+	private final Lock queueLock=new ReentrantLock();
+	
+	/**
+	 * Method that prints a document
+	 * @param document document to print
+	 */
+	public void printJob(Object document){
+		queueLock.lock();
+		
+		try {
+			Long duration=(long)(Math.random()*10000);
+			System.out.printf("%s: PrintQueue: Printing a Job during %d seconds\n",Thread.currentThread().getName(),(duration/1000));
+			Thread.sleep(duration);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		} finally {
+			queueLock.unlock();
+		}
+	}
+}
