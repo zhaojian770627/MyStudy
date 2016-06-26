d974660be4b6e62a7953aefeff0f98199e934ed6
zhaojianc <zhaojianc@yonyou.com>
2016-06-26 21:41:57 +0800

    s7
    Signed-off-by: zhaojianc <zhaojianc@yonyou.com>

--- MyStudy/src/testcase/TestCal.java
+++ MyStudy/src/testcase/TestCal.java
@@ -3,9 +3,18 @@ package testcase;
 import junit.framework.TestCase;
 
 public class TestCal extends TestCase {
-	public void testAdd(){ 
-        Calcuator calcuator=new Calcuator(); 
-        double result=calcuator.add(1,2); 
-        assertEquals(3,result,0); 
-    } 
+	public void testAdd() {
+		Calcuator calcuator = new Calcuator();
+		double result = calcuator.add(1, 2);
+		assertEquals(3, result, 0);
+	}
+
+	public void testXml() throws Exception
+	{
+		String ss = new SSCTaskPushMQWfGadgetHelper().generateXml("aaaa", "bbbb", "ccc", "billid");
+		
+		new SSCTaskPushMQWfGadgetHelper().executeOperate(ss);;
+		int i=0;
+		i++;
+	}
 }
