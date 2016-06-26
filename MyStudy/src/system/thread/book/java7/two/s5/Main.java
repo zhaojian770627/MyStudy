5a0cd1c9b4fb5b1cb4a4126f473909a70aeb8a12
zhaojianc <zhaojianc@yonyou.com>
2016-06-26 20:36:34 +0800

    s5
    Signed-off-by: zhaojianc <zhaojianc@yonyou.com>

---
+++ MyStudy/src/system/thread/book/java7/two/s5/Main.java
@@ -0,0 +1,39 @@
+package system.thread.book.java7.two.s5;
+
+/**
+ * Main class of the example
+ *
+ */
+public class Main {
+
+	/**
+	 * Main class of the example
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+		// Creates an object to store the prices
+		PricesInfo pricesInfo=new PricesInfo();
+		
+		Reader readers[]=new Reader[5];
+		Thread threadsReader[]=new Thread[5];
+		
+		// Creates five readers and threads to run them
+		for (int i=0; i<5; i++){
+			readers[i]=new Reader(pricesInfo);
+			threadsReader[i]=new Thread(readers[i]);
+		}
+		
+		// Creates a writer and a thread to run it
+		Writer writer=new Writer(pricesInfo);
+		Thread threadWriter=new Thread(writer);
+		
+		// Starts the threads
+		for (int i=0; i<5; i++){
+			threadsReader[i].start();
+		}
+		threadWriter.start();
+		
+	}
+
+}
