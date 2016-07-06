d6f112121f01586e47f0fa79c759c86a04f14848
zhaojianc <zhaojianc@yonyou.com>
2016-07-06 21:46:58 +0800

    add java
    Signed-off-by: zhaojianc <zhaojianc@yonyou.com>

---
+++ MyStudy/src/system/thread/book/java7/three/s4/Main.java
@@ -0,0 +1,50 @@
+package system.thread.book.java7.three.s4;
+
+import java.util.concurrent.CyclicBarrier;
+
+/**
+ * Main class of the example
+ *
+ */
+public class Main {
+
+	/**
+	 * Main method of the example
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+		/*
+		 * Initializes the bi-dimensional array of data
+		 * 		10000 rows
+		 * 		1000 numbers in each row
+		 * 		Looking for number 5
+		 */
+		final int ROWS=10000;
+		final int NUMBERS=1000;
+		final int SEARCH=5; 
+		final int PARTICIPANTS=5;
+		final int LINES_PARTICIPANT=2000;
+		MatrixMock mock=new MatrixMock(ROWS, NUMBERS,SEARCH);
+		
+		// Initializes the object for the results
+		Results results=new Results(ROWS);
+		
+		// Creates an Grouper object
+		Grouper grouper=new Grouper(results);
+		
+		// Creates the CyclicBarrier object. It has 5 participants and, when
+		// they finish, the CyclicBarrier will execute the grouper object
+		CyclicBarrier barrier=new CyclicBarrier(PARTICIPANTS,grouper);
+		
+		// Creates, initializes and starts 5 Searcher objects
+		Searcher searchers[]=new Searcher[PARTICIPANTS];
+		for (int i=0; i<PARTICIPANTS; i++){
+			searchers[i]=new Searcher(i*LINES_PARTICIPANT, (i*LINES_PARTICIPANT)+LINES_PARTICIPANT, mock, results, 5,barrier);
+			Thread thread=new Thread(searchers[i]);
+			thread.start();
+		}
+		System.out.printf("Main: The main thread has finished.\n");
+	}
+
+}
