fa3e152c11334487bc00a57557e743a96b96f7a0
zhaojianc <zhaojianc@yonyou.com>
2016-07-10 14:21:34 +0800

    add java
    Signed-off-by: zhaojianc <zhaojianc@yonyou.com>

---
+++ springweb/WEB-INF/src/springweb/remoteclient/RemoteMain.java
@@ -0,0 +1,47 @@
+package springweb.remoteclient;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import springweb.model.Spitter;
+import springweb.service.SpitterService;
+
+public class RemoteMain {
+
+	public static void main(String[] args) {
+		//callRmi();
+		//callHessian();
+		//callbulap();
+		callhttpInvoker();
+	}
+	
+	private static void callhttpInvoker() {
+		ApplicationContext context = new ClassPathXmlApplicationContext("httpInvoker-client-context.xml", RemoteMain.class); // <co
+		SpitterService spitterService = (SpitterService) context.getBean("spitterService");
+		Spitter spitter=spitterService.getSpitter(44);
+		System.out.println(spitter.getUsername());		
+	}
+	
+	private static void callbulap() {
+		ApplicationContext context = new ClassPathXmlApplicationContext("burlap-client-context.xml", RemoteMain.class); // <co
+		SpitterService spitterService = (SpitterService) context.getBean("spitterService");
+		Spitter spitter=spitterService.getSpitter(44);
+		System.out.println(spitter.getUsername());		
+	}
+	
+	private static void callHessian() {
+		ApplicationContext context = new ClassPathXmlApplicationContext("hessian-client-context.xml", RemoteMain.class); // <co
+		SpitterService spitterService = (SpitterService) context.getBean("spitterService");
+		Spitter spitter=spitterService.getSpitter("zhaojian");
+		System.out.println(spitter.getUsername());		
+	}
+
+	static void callRmi()
+	{
+		ApplicationContext context = new ClassPathXmlApplicationContext("rmi-client-context.xml", RemoteMain.class); // <co
+		SpitterService spitterService = (SpitterService) context.getBean("spitterService");
+		Spitter spitter=spitterService.getSpitter(44);
+		System.out.println(spitter.getUsername());
+	}
+
+}
