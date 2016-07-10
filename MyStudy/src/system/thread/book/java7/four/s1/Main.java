f5f0fac75172448ed0adf3e56bfbfe30b52c314f
zhaojianc <zhaojianc@yonyou.com>
2016-07-10 17:00:48 +0800

    add
    Signed-off-by: zhaojianc <zhaojianc@yonyou.com>

---
+++ MyStudy/src/system/thread/book/java7/four/s1/Main.java
@@ -0,0 +1,28 @@
+package system.thread.book.java7.four.s1;
+
+/**
+ * Main class of the example. Creates a server and 100 request of the Task class
+ * that sends to the server
+ *
+ */
+public class Main {
+
+	/**
+	 * Main method of the example
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// Create the server
+		Server server=new Server();
+		
+		// Send 100 request to the server and finish
+		for (int i=0; i<100; i++){
+			Task task=new Task("Task "+i);
+			server.executeTask(task);
+		}
+		
+		server.endServer();
+
+	}
+
+}
