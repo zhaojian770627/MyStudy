937a787e90db9ea16c5855efbe25b7b7e2261ba8
zhaojianc <zhaojianc@yonyou.com>
2016-07-10 16:11:49 +0800

    add java file
    Signed-off-by: zhaojianc <zhaojianc@yonyou.com>

--- springweb/WEB-INF/src/springweb/remoteclient/RemoteMain.java
+++ springweb/WEB-INF/src/springweb/remoteclient/RemoteMain.java
@@ -12,9 +12,17 @@ public class RemoteMain {
 		//callRmi();
 		//callHessian();
 		//callbulap();
-		callhttpInvoker();
+		//callhttpInvoker();
+		callws();
 	}
 	
+	private static void callws() {
+		ApplicationContext context = new ClassPathXmlApplicationContext("jaxws-client-context.xml", RemoteMain.class); // <co
+		SpitterService spitterService = (SpitterService) context.getBean("spitterService");
+		Spitter spitter=spitterService.getSpitter(44);
+		System.out.println(spitter.getUsername());			
+	}
+
 	private static void callhttpInvoker() {
 		ApplicationContext context = new ClassPathXmlApplicationContext("httpInvoker-client-context.xml", RemoteMain.class); // <co
 		SpitterService spitterService = (SpitterService) context.getBean("spitterService");
