192b8666c61bb513d48f07f85c7a947164039240
zhaojianc <zhaojianc@yonyou.com>
2016-07-24 20:24:31 +0800

    add
    Signed-off-by: zhaojianc <zhaojianc@yonyou.com>

---
+++ MyStudy/src/system/thread/book/java7/eight/s5/Main.java
@@ -0,0 +1,62 @@
+package system.thread.book.java7.eight.s5;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * Main class of the example. It launch five Task objects and write
+ * some log messages indicating the evolution of the execution of the program
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		/*
+		 * Get the Logger object
+		 */
+		Logger logger=MyLogger.getLogger("Core");
+		
+		/*
+		 * Write a message indicating the start of the execution
+		 */
+		logger.entering("Core", "main()",args);
+		
+		/*
+		 * Create and launch five Task objects
+		 */
+		Thread threads[]=new Thread[5];
+		for (int i=0; i<threads.length; i++) {
+			logger.log(Level.INFO,"Launching thread: "+i);
+			Task task=new Task();
+			threads[i]=new Thread(task);
+			logger.log(Level.INFO,"Thread created: "+threads[i].getName());
+			threads[i].start();
+		}
+		
+		/*
+		 * Write a log message indicating that the threads have been created
+		 */
+		logger.log(Level.INFO,"Ten Threads created. Waiting for its finalization");
+		
+		/*
+		 * Wait for the finalization of the threads
+		 */
+		for (int i=0; i<threads.length; i++) {
+			try {
+				threads[i].join();
+				logger.log(Level.INFO,"Thread has finished its execution",threads[i]);
+			} catch (InterruptedException e) {
+				logger.log(Level.SEVERE, "Exception", e);
+			}
+		}
+		
+		/*
+		 * Write a log message indicating the end of the program
+		 */
+		logger.exiting("Main", "main()");
+	}
+
+}
