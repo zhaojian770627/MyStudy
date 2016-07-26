9d5a7481241940f17d2d813930eb9740662b6844
zhaojianc <zhaojianc@yonyou.com>
2016-07-26 14:05:56 +0800

    add
    Change-Id: I0e52ebd9742bf537e4b0759cc7dcf082e1cb1508
    Signed-off-by: zhaojianc <zhaojianc@yonyou.com>

---
+++ MyStudy/src/system/thread/book/java7/eight/s7/Main.java
@@ -0,0 +1,29 @@
+package system.thread.book.java7.eight.s7;
+
+import edu.umd.cs.mtc.TestFramework;
+
+/**
+ * Main class of the example. Executes the test of the LinkedTransferQueue
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) throws Throwable {
+		
+		/*
+		 * Create a Test object
+		 */
+		ProducerConsumerTest test=new ProducerConsumerTest();
+		
+		/*
+		 * Execute the test
+		 */
+		System.out.printf("Main: Starting the test\n");
+		TestFramework.runOnce(test);
+		System.out.printf("Main: The test has finished\n");
+
+	}
+
+}
