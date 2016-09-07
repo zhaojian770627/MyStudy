bd3ffe175ab91092743f5fce1ed2b75fb3e4aee8
zhaojianc <zhaojianc@zhaojianc-PC>
2016-09-07 22:26:31 +0800

    add
    Signed-off-by: zhaojianc <zhaojianc@zhaojianc-PC>

---
+++ MyStudy/src/system/jpa/EmployeeService.java
@@ -0,0 +1,44 @@
+package system.jpa;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+
+public class EmployeeService {
+	protected EntityManager em;
+
+	public EmployeeService(EntityManager em) {
+		this.em = em;
+	}
+
+	public Employee createEmployee(int id, String name, long salary) {
+		Employee emp = new Employee(id);
+		emp.setName(name);
+		emp.setSalary(salary);
+		em.persist(emp);
+		return emp;
+	}
+
+	public void removeEmployee(int id) {
+		Employee emp = findEmployee(id);
+		if (emp != null)
+			em.remove(emp);
+	}
+
+	public Employee raiseEmployeeSalary(int id, long raise) {
+		Employee emp = new Employee(id);
+		if (emp != null)
+			emp.setSalary(emp.getSalary() + raise);
+		return emp;
+	}
+
+	public Employee findEmployee(int id) {
+		return em.find(Employee.class, id);
+	}
+
+	public List<Employee> findAllEmployees() {
+		TypedQuery<Employee> query = em.createQuery("select e from Employee e", Employee.class);
+		return query.getResultList();
+	}
+}
--- MyStudy/src/system/jpa/TestJPA.java
+++ MyStudy/src/system/jpa/TestJPA.java
@@ -1,12 +1,48 @@
 package system.jpa;
 
+import java.util.List;
+
+import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
 public class TestJPA {
 
 	public static void main(String[] args) {
-		EntityManagerFactory emf=Persistence.createEntityManagerFactory("EmployeeService");
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("EmployeeService");
+		EntityManager em = emf.createEntityManager();
+		EmployeeService service = new EmployeeService(em);
+
+		// Create and persist an employee
+		em.getTransaction().begin();
+		Employee emp = service.createEmployee(158, "John Doe", 45000);
+		em.getTransaction().commit();
+		System.out.println("Persisted " + emp);
+
+		// find a specitic employee
+		emp = service.findEmployee(158);
+		System.out.println("Found " + emp);
+
+		// find all employees
+		List<Employee> emps = service.findAllEmployees();
+		for (Employee e : emps)
+			System.out.println("Found employee: " + e);
+
+		// update the employee
+		em.getTransaction().begin();
+		emp = service.raiseEmployeeSalary(158, 1000);
+		em.getTransaction().commit();
+		System.out.println("Updated " + emp);
+
+		// remove an employee
+		em.getTransaction().begin();
+		service.removeEmployee(158);
+		em.getTransaction().commit();
+		System.out.println("Removed Employee 158");
+
+		// close the EM and EMF when done
+		em.close();
+		emf.close();
 	}
 
 }
