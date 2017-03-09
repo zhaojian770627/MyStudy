0d53df4589383c60b57cf96b90d2f923d6331a39
zhaojianc <zhaojianc@yonyou.com>
2017-03-09 19:48:12 +0800

    add
    Change-Id: I0b6911ecac19105c4ea40fceb19d640c37685f33
    Signed-off-by: zhaojianc <zhaojianc@yonyou.com>

---
+++ StudyWeb/WebContent/js/mvc/assert.js
@@ -0,0 +1,9 @@
+var assert = function(value, msg) {
+	if (!value)
+		throw (msg || (value + " does not equal true"))
+};
+
+var assertEqual = function(val1, val2, msg) {
+	if (val1 != val2)
+		throw (msg || (val1 + " does not equal " + val2));
+};
---
+++ StudyWeb/WebContent/js/mvc/class.js
@@ -0,0 +1,44 @@
+var Class = function(parent) {
+	var klass = function() {
+		this.init.apply(this, arguments);
+	};
+
+	// 改变klass的原型
+	if (parent) {
+		var subclass = function() {
+		};
+		subclass.prototype = parent.prototype;
+		klass.prototype = new subclass;
+	};
+
+	klass.prototype.init = function() {
+	};
+
+	// 定义 prototype 的别名
+	klass.fn = klass.prototype;
+
+	// 定义类的别名
+	klass.fn.parent = klass;
+
+	// 给类添加属性
+	klass.extend = function(obj) {
+		var extended = obj.extended;
+		for ( var i in obj) {
+			klass[i] = obj[i];
+		}
+		if (extended)
+			extended(klass);
+	};
+
+	// 为实例添加属性
+	klass.include = function(obj) {
+		var included = obj.included;
+		for ( var i in obj) {
+			klass.fn[i] = obj[i];
+		}
+		if (included)
+			included(kclass)
+	};
+
+	return klass;
+};
