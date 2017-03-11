c711e636933cfc04ffba0c35bc3ede01a56d5105
zhaojianc <zhaojianc@yonyou.com>
2017-03-11 15:51:05 +0800

    add
    Change-Id: Ide737444022bdb0d2fe6826bf5ae1d80edfe6561
    Signed-off-by: zhaojianc <zhaojianc@yonyou.com>

--- StudyWeb/WebContent/js/mvc/model.js
+++ StudyWeb/WebContent/js/mvc/model.js
@@ -6,6 +6,13 @@ if (typeof Object.create !== "function")
 		return new F();
 	};
 
+Math.guid = function() {
+	return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+		var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+		return v.toString(16);
+	}).toUpperCase();
+}
+
 var Model = {
 	inherited : function() {
 	},
@@ -34,5 +41,56 @@ var Model = {
 		instance.parent = this;
 		instance.init.apply(instance, arguments);
 		return instance;
+	},
+
+	extend : function(o) {
+		var extended = o.extended;
+		jQuery.extend(this, o);
+		if (extended)
+			extended(this);
+	},
+
+	include : function(o) {
+		var included = o.included;
+		jQuery.extend(this.prototype, o);
+		if (included)
+			included(this);
 	}
 };
+
+// -------------------------------------------------------
+Model.records = {};
+
+Model.include({
+	newRecord : true,
+
+	create : function() {
+		if (!this.id)
+			this.id = Math.guid();
+
+		this.newRecord = false;
+		this.parent.records[this.id] = this;
+	},
+
+	destroy : function() {
+		delete this.parent.records[this.id];
+	},
+
+	update : function() {
+		this.parent.records[this.id] = this;
+	},
+
+	save : function() {
+		this.newRecord ? this.create() : this.update();
+	}
+});
+
+Model.extend({
+	created : function() {
+		this.records = {};
+	},
+
+	find : function(id) {
+		return this.records[id] || "Unknown record";
+	}
+});
