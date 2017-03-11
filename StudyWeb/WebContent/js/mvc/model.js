fffa6b999830794ea0eaf830ac612743690a6b23
zhaojianc <zhaojianc@yonyou.com>
2017-03-11 16:57:13 +0800

    add
    Change-Id: Icf11f2ded63d35164f15cc46ebbb709d517bf2a1
    Signed-off-by: zhaojianc <zhaojianc@yonyou.com>

--- StudyWeb/WebContent/js/mvc/model.js
+++ StudyWeb/WebContent/js/mvc/model.js
@@ -82,15 +82,65 @@ Model.include({
 
 	save : function() {
 		this.newRecord ? this.create() : this.update();
+	},
+
+	attributes : function() {
+		var result = {};
+		for ( var i in this.parent.attributes) {
+			var attr = this.parent.attributes[i];
+			result[attr] = this[attr];
+		}
+		result.id = this.id;
+		return result;
+	},
+
+	toJSON : function() {
+		return (this.attributes());
+	},
+
+	createRemote : function(url, callback) {
+		$.post(url, this.attributes(), callback);
+	},
+
+	updateRemote : function(url, callback) {
+		$.ajax({
+			url : url,
+			data : this.attributes(),
+			success : callback,
+			type : "PUT"
+		});
 	}
 });
 
 Model.extend({
 	created : function() {
 		this.records = {};
+		this.attributes = [];
 	},
 
 	find : function(id) {
 		return this.records[id] || "Unknown record";
+	},
+
+	populate : function(values) {
+		this.records = {};
+		for (var i = 0, il = values.length; i < il; i++) {
+			var record = this.init(values[i]);
+			record.newRecord = false;
+			this.records[record.id] = record;
+		}
 	}
 });
+
+Model.LocalStorage = {
+	saveLocal : function(name) {
+		var result = [];
+		for ( var i in this.records)
+			result.push(this.records[i]);
+		localStorage[name] = JSON.stringify(result);
+	},
+	loadLocal : function(name) {
+		var result = JSON.parse(localStorage[name]);
+		this.populate(result);
+	}
+};
