8d82b553267a4bd4b75cb1b9abaa988d74e9629a
zhaojianc <zhaojianc@yonyou.com>
2017-03-14 16:08:54 +0800

    ddd
    Change-Id: I35e9682e7c809f295441ca6f60b3be4eaa262588
    Signed-off-by: zhaojianc <zhaojianc@yonyou.com>

--- StudyWeb/WebContent/js/mvc/ctrols.js
+++ StudyWeb/WebContent/js/mvc/ctrols.js
@@ -1,23 +1,37 @@
-(function($, exports) {
-	var mod = function(includes) {
+// 使用全局对象作为上下文，而不是window对象
+// 用来创建全局对象
+var exports = this;
+
+(function($) {
+	var mod = {};
+
+	mod.create = function(includes) {
+		var result = function() {
+			this.init.apply(this, arguments);
+		};
+
+		result.fn = result.prototype;
+		result.fn.init = function() {
+		};
+
+		result.proxy = function(func) {
+			return $.proxy(func, this);
+		};
+		result.fn.proxy = result.proxy;
+
+		result.include = function(obj) {
+			$.extend(this.fn, obj);
+		};
+
+		result.extend = function(obj) {
+			$.extend(this, obj);
+		};
+
 		if (includes)
-			this.include(includes);
-	};
+			result.include(includes);
 
-	mod.fn = mod.prototype;
-
-	mod.fn.proxy = function(func) {
-		return $.proxy(func, this);
-	};
-
-	mod.fn.load = function(func) {
-		$(this.proxy(func));
-	};
-
-	mod.fn.include = function(obj) {
-		$.extend(this, obj);
+		return result;
 	};
 
 	exports.Controller = mod;
-
-})(jQuery, window);
+})(jQuery);
