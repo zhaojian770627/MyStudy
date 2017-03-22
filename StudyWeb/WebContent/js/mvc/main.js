c6e001d9ca962186dce30f75ed4be068e92af845
zhaojianc <zhaojianc@nczhaojianc.ufsoft.com.cn>
2017-03-22 10:40:13 +0800

    aaaa
    Signed-off-by: zhaojianc <zhaojianc@nczhaojianc.ufsoft.com.cn>

--- StudyWeb/WebContent/js/mvc/main.js
+++ StudyWeb/WebContent/js/mvc/main.js
@@ -1,6 +1,11 @@
-require([ 'sth', 'check' ], function(sth, check) {
+require([ 'sth', 'check', 'jquery' ], function(sth, check, $) {
 	if (check.ok) {
-		sth.f1()
+		sth.f1();
+		$(document).ready(function() {
+			$("#p1").click(function() {
+				$(this).hide();
+			});
+		});
 	} else {
 		sth.f2()
 	}
