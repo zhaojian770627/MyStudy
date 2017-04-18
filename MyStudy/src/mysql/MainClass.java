3dbf5559b7cfbe872b105f090457775ff23d0f43
zhaojianc <zhaojianc@nczhaojianc.ufsoft.com.cn>
2017-04-18 19:07:26 +0800

    up
    Signed-off-by: zhaojianc <zhaojianc@nczhaojianc.ufsoft.com.cn>

--- MyStudy/src/mysql/MainClass.java
+++ MyStudy/src/mysql/MainClass.java
@@ -24,49 +24,53 @@ public class MainClass {
 
 		String[] dbs = new String[] { "chat", "jan0226q", "kv44m66g", "q46yu5wz", "q4v4r2ki", "qk59sr36", "r5gumlrc",
 				"rxjqedzo", "sqoy16xa", "sxvhovwv", "t1ex71tt", "trp1wo0y", "upvy65zj", "v77svmci", "vw9xwznp",
-				"wfb0deol", "x8z0s8lv", "xayk3ux7", "xx9i3hpm", "z8tfmb1j", "zpqtux0l" };
+				"wfb0deol", "x8z0s8lv", "xayk3ux7", "xx9i3hpm", "z8tfmb1j", "zpqtux0l", "ysdzxt76" };
 		// String[] dbs = new String[] { "jbpm3" };
 		Class.forName(driver).newInstance();
 		Connection conn = null;
 		Statement stmt = null;
 
-		PrintWriter pw = new PrintWriter(System.err);
+		File file = new File("D:\\Project\\ybz\\upcoderule.sql");
+		PrintWriter pwerror = new PrintWriter(System.err);
+		PrintWriter pwlog = new PrintWriter(System.out);
 
-		
-		
-		for (String db : dbs) {
-			try {
-				File file = new File("D:\\Project\\ybz\\upcoderule.sql");
+		try {
+			conn = DriverManager.getConnection(url, username, password);
+			for (String db : dbs) {
 				FileReader fileReader = new FileReader(file);
-				
+
 				System.out.println("Execute " + db);
-				conn = DriverManager.getConnection(url, username, password);
 				stmt = conn.createStatement();
 				ScriptRunner runner = new ScriptRunner(conn);
 
-				runner.setErrorLogWriter(pw);
-				runner.setLogWriter(pw);
+				runner.setErrorLogWriter(pwerror);
+				runner.setLogWriter(pwlog);
 
 				conn.setCatalog(db);
 				// runner.runScript(Resources.getResourceAsReader("test.sql"));
 				Resources.setCharset(Charset.forName("utf-8"));
 				runner.runScript(fileReader);
 				fileReader.close();
-			} catch (Exception e) {
-				e.printStackTrace();
-			} finally {
 				try {
 					if (stmt != null) {
 						stmt.close();
-					}
-					if (conn != null) {
-						conn.close();
 					}
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
 			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (conn != null) {
+					conn.close();
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
 		}
+
 	}
 
 }
