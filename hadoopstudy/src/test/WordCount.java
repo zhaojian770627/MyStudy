d5850c01d719a351b8ba91390c563cef5a50cce6
zhaojianc <zhaojianc@yonyou.com>
2017-10-30 14:31:58 +0800

    add
    Signed-off-by: zhaojianc <zhaojianc@yonyou.com>

--- hadoopstudy/src/test/WordCount.java
+++ hadoopstudy/src/test/WordCount.java
@@ -1,4 +1,6 @@
 package test;
+
+import java.io.File;
 import java.io.IOException;
 import java.util.StringTokenizer;
 
@@ -7,6 +9,7 @@ import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
+import org.apache.hadoop.io.compress.GzipCodec;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
@@ -17,51 +20,73 @@ import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
 
 /**
  * Hadoop - 统计文件单词出现频次
+ * 
  * @author antgan
  *
  */
 public class WordCount {
-    public static class WordCountMap extends
-            Mapper<LongWritable, Text, Text, IntWritable> {
-        private final IntWritable one = new IntWritable(1);
-        private Text word = new Text();
+	public static class WordCountMap extends Mapper<LongWritable, Text, Text, IntWritable> {
+		private final IntWritable one = new IntWritable(1);
+		private Text word = new Text();
 
-        public void map(LongWritable key, Text value, Context context)
-                throws IOException, InterruptedException {
-            String line = value.toString();
-            StringTokenizer token = new StringTokenizer(line);
-            while (token.hasMoreTokens()) {
-                word.set(token.nextToken());
-                context.write(word, one);
-            }
-        }
-    }
+		public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+			String line = value.toString();
+			StringTokenizer token = new StringTokenizer(line);
+			while (token.hasMoreTokens()) {
+				word.set(token.nextToken());
+				context.write(word, one);
+			}
+		}
+	}
 
-    public static class WordCountReduce extends
-            Reducer<Text, IntWritable, Text, IntWritable> {
-        public void reduce(Text key, Iterable<IntWritable> values,
-                Context context) throws IOException, InterruptedException {
-            int sum = 0;
-            for (IntWritable val : values) {
-                sum += val.get();
-            }
-            context.write(key, new IntWritable(sum));
-        }
-    }
+	public static class WordCountReduce extends Reducer<Text, IntWritable, Text, IntWritable> {
+		public void reduce(Text key, Iterable<IntWritable> values, Context context)
+				throws IOException, InterruptedException {
+			int sum = 0;
+			for (IntWritable val : values) {
+				sum += val.get();
+			}
+			context.write(key, new IntWritable(sum));
+		}
+	}
 
-    public static void main(String[] args) throws Exception {
-        Configuration conf = new Configuration();
-        Job job = new Job(conf);
-        job.setJarByClass(WordCount.class);
-        job.setJobName("wordcount");
-        job.setOutputKeyClass(Text.class);
-        job.setOutputValueClass(IntWritable.class);
-        job.setMapperClass(WordCountMap.class);
-        job.setReducerClass(WordCountReduce.class);
-        job.setInputFormatClass(TextInputFormat.class);
-        job.setOutputFormatClass(TextOutputFormat.class);
-        FileInputFormat.addInputPath(job, new Path(args[0]));
-        FileOutputFormat.setOutputPath(job, new Path(args[1]));
-        job.waitForCompletion(true);
-    }
+	public static void main(String[] args) throws Exception {
+		if(args.length<2)
+			throw new Exception("参数必须为２");
+		
+		Configuration conf = new Configuration();
+		Job job = new Job(conf);
+		job.setJarByClass(WordCount.class);
+		job.setJobName("wordcount");
+		job.setOutputKeyClass(Text.class);
+		job.setOutputValueClass(IntWritable.class);
+		job.setMapperClass(WordCountMap.class);
+		job.setReducerClass(WordCountReduce.class);
+		job.setInputFormatClass(TextInputFormat.class);
+		job.setOutputFormatClass(TextOutputFormat.class);
+		FileInputFormat.addInputPath(job, new Path(args[0]));
+		FileOutputFormat.setOutputPath(job, new Path(args[1]));
+
+		deleteAllFilesOfDir(new File(args[1]));
+
+		// 使用输出压缩
+		FileOutputFormat.setCompressOutput(job, true);
+		FileOutputFormat.setOutputCompressorClass(job, GzipCodec.class);
+
+		job.waitForCompletion(true);
+	}
+
+	public static void deleteAllFilesOfDir(File path) {
+		if (!path.exists())
+			return;
+		if (path.isFile()) {
+			path.delete();
+			return;
+		}
+		File[] files = path.listFiles();
+		for (int i = 0; i < files.length; i++) {
+			deleteAllFilesOfDir(files[i]);
+		}
+		path.delete();
+	}
 }
