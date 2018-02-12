8e426cb4e1fbf58a269ebc2ee0940f1539a237d2
zhaojian770627 <zhaojian770627@163.com>
2018-02-12 16:42:51 +0800

    aaa
    Signed-off-by: zhaojian770627 <zhaojian770627@163.com>

--- JavaStudy/src/main/java/math3/Main.java
+++ JavaStudy/src/main/java/math3/Main.java
@@ -5,14 +5,14 @@ import org.apache.commons.math3.linear.RealMatrix;
 
 public class Main {
 
-        public static void main(String[] args) {
-                double m1[][] = new double[][] { { 1, 2, 3 } };
-                double m2[][] = new double[][] { { 1 }, { 2 }, { 3 } };
+	public static void main(String[] args) {
+		double m1[][] = new double[][] { { 1, 2, 3 } };
+		double m2[][] = new double[][] { { 1 }, { 2 }, { 3 } };
 
-                RealMatrix matrix1 = new Array2DRowRealMatrix(m1);
-                RealMatrix matrix2 = new Array2DRowRealMatrix(m2);
-                System.out.println(matrix1.multiply(matrix2));
+		RealMatrix matrix1 = new Array2DRowRealMatrix(m1);
+		RealMatrix matrix2 = new Array2DRowRealMatrix(m2);
+		System.out.println(matrix1.multiply(matrix2));
 
-        }
+	}
 
 }
