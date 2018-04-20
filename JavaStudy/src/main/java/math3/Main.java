638309208e15ca3aae9925868c4842283937d78a
zhaojian770627 <zhaojian770627@163.com>
2018-04-20 19:17:28 +0800

    add
    Signed-off-by: zhaojian770627 <zhaojian770627@163.com>

--- JavaStudy/src/main/java/math3/Main.java
+++ JavaStudy/src/main/java/math3/Main.java
@@ -1,17 +1,40 @@
 package math3;
 
-import org.apache.commons.math3.linear.Array2DRowRealMatrix;
-import org.apache.commons.math3.linear.RealMatrix;
+import cern.colt.matrix.DoubleMatrix2D;
+import cern.colt.matrix.impl.DenseDoubleMatrix2D;
+import cern.colt.matrix.linalg.Algebra;
 
 public class Main {
 
 	public static void main(String[] args) {
-		double m1[][] = new double[][] { { 1, 2, 3 } };
-		double m2[][] = new double[][] { { 1 }, { 2 }, { 3 } };
+//		double m1[][] = new double[][] { { 1, 2, 3 } };
+//		double m2[][] = new double[][] { { 1 }, { 2 }, { 3 } };
+//
+//		RealMatrix matrix1 = new Array2DRowRealMatrix(m1);
+//		RealMatrix matrix2 = new Array2DRowRealMatrix(m2);
+//		System.out.println(matrix1.multiply(matrix2));
 
-		RealMatrix matrix1 = new Array2DRowRealMatrix(m1);
-		RealMatrix matrix2 = new Array2DRowRealMatrix(m2);
-		System.out.println(matrix1.multiply(matrix2));
+		algebraSolve();
+	}
+
+	/*
+	 * 解方程
+	 */
+	private static void algebraSolve() {
+		DoubleMatrix2D matrix = new DenseDoubleMatrix2D(2, 2);
+		matrix.set(0, 0, 3.0);
+		matrix.set(0, 1, 5.0);
+		matrix.set(1, 0, 4.0);
+		matrix.set(1, 1, 2.0);
+
+		DoubleMatrix2D D;
+		D = new DenseDoubleMatrix2D(2, 1);
+		D.set(0, 0, 16);
+		D.set(1, 0, 18);
+		DoubleMatrix2D X;
+		Algebra algebra = new Algebra();
+		X = algebra.solve(matrix, D);
+		System.out.println(X);
 
 	}
 
